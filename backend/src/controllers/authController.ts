1512299717ceeb84d86dcda45f75309bc537bd64
Ednan Rogério Frizzera Filho <ednan@frizzera.dev>
2025-11-26 10:23:49 -0300

    feat: implement account creation controller

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,7 +1,44 @@
 import { Request, Response, NextFunction } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import {
+    ApiResponse,
+    UserRegistrationBody,
+    UserRegistrationSuccess,
+} from '../types/api.js';
+import User, { IUserDocument } from '../models/User.js';
 
-const registerUser = (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).json({ message: 'register route hit' });
+const registerUser = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
+    const { email, password }: UserRegistrationBody = req.body; // Validated with zod
+
+    try {
+        // Create new user in the DB
+        const newUser: IUserDocument = await User.create({
+            email,
+            password,
+        });
+
+        // Generate a new token to be sent
+        const token = await newUser.createJWT();
+
+        // Generate the response
+        const response: ApiResponse<UserRegistrationSuccess> = {
+            status: 'success',
+            data: {
+                message: 'Account created. Welcome aboard!',
+                id: newUser._id.toString(),
+                email: newUser.email,
+                token: token,
+            },
+        };
+
+        res.status(StatusCodes.OK).json(response);
+    } catch (e) {
+        next(e);
+    }
 };
 
 const loginUser = (req: Request, res: Response, next: NextFunction) => {
--- backend/src/types/api.ts
+++ backend/src/types/api.ts
@@ -1,3 +1,5 @@
+import { z } from 'zod';
+import { userRegisterSchema } from '../zodSchemas/user.js';
 // Generic types
 export interface ApiResponse<T = never> {
     status: 'success' | 'error';
@@ -11,4 +13,18 @@ export interface ApiError {
 export interface MongoDatabaseError {
     code: number;
     message: string;
+}
+
+// Authentication types
+
+export type UserRegistrationBody = z.infer<typeof userRegisterSchema>;
+
+export interface UserBase {
+    message: string;
+    email: string;
+}
+
+export interface UserRegistrationSuccess extends UserBase {
+    id: string;
+    token: string;
 }
