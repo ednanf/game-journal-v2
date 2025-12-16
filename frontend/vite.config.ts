99928da83ee525faecd6fa30fd59856d49589a06
Ednan Rogério Frizzera Filho <ednan@frizzera.dev>
2025-12-16 11:38:04 -0300

    chore: add config to use ngrok

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
-})
+    plugins: [
+        react({
+            babel: {
+                plugins: [['babel-plugin-react-compiler']],
+            },
+        }),
+    ],
+    server: {
+        allowedHosts: ['choice-joint-ghost.ngrok-free.app'],
+        proxy: {
+            '/api/v1': {
+                target: 'http://localhost:5173',
+                changeOrigin: true,
+            },
+        },
+    },
+});
