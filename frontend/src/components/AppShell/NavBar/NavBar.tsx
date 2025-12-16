f635b03f93b7bd8b133bb5a9e7f3cf6e749377af
Ednan Rogério Frizzera Filho <ednan@frizzera.dev>
2025-12-16 11:38:21 -0300

    feat: implement navbar

--- frontend/src/components/AppShell/NavBar/NavBar.tsx
+++ frontend/src/components/AppShell/NavBar/NavBar.tsx
@@ -3,6 +3,8 @@ import { HStack } from 'react-swiftstacks';
 import NavBarButton from './NavBarButton/NavBarButton.tsx';
 
 import { BsJournalBookmarkFill, BsBarChartFill } from 'react-icons/bs';
+import { FaMagnifyingGlass } from 'react-icons/fa6';
+import { HiOutlineCog } from 'react-icons/hi';
 import { GoPlus } from 'react-icons/go';
 
 import styles from './NavBar.module.css';
@@ -15,12 +17,20 @@ const NavBar = () => {
                     Journal
                 </NavBarButton>
 
+                <NavBarButton to={'search'} icon={<FaMagnifyingGlass />}>
+                    Search
+                </NavBarButton>
+
                 <NavBarButton to={'addEntry'} icon={<GoPlus />}>
                     Add Entry
                 </NavBarButton>
 
                 <NavBarButton to={'statistics'} icon={<BsBarChartFill />}>
                     Statistics
+                </NavBarButton>
+
+                <NavBarButton to={'settings'} icon={<HiOutlineCog />}>
+                    Settings
                 </NavBarButton>
             </div>
         </HStack>
