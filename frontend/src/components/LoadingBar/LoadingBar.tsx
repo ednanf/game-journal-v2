68e1433cee507dbff4570c7e19b750c84a5eb947
Ednan Rogério Frizzera Filho <ednan@frizzera.dev>
2026-01-13 13:10:31 -0300

    feat: change loading animation for a neumorphic style
    Signed-off-by: Ednan Rogério Frizzera Filho <ednan@frizzera.dev>

--- frontend/src/components/LoadingBar/LoadingBar.tsx
+++ frontend/src/components/LoadingBar/LoadingBar.tsx
@@ -1,6 +1,13 @@
 import styles from './LoadingBar.module.css';
 
 const LoadingBar = () => {
-    return <div className={styles.loader}></div>;
+    return (
+        <div className={styles.loader}>
+            <div className={styles.item1}></div>
+            <div className={styles.item2}></div>
+            <div className={styles.item3}></div>
+        </div>
+    );
 };
+
 export default LoadingBar;
