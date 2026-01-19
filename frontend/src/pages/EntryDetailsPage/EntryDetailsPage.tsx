5b45b3bebdd4d0a74f9db0c3b0481fed2fc7ff78
Ednan Rogério Frizzera Filho <ednan@frizzera.dev>
2026-01-19 12:02:17 -0300

    feat: refactor EntryDetailsPage.tsx to have a cleaner conditional loading state
    Signed-off-by: Ednan Rogério Frizzera Filho <ednan@frizzera.dev>

--- frontend/src/pages/EntryDetailsPage/EntryDetailsPage.tsx
+++ frontend/src/pages/EntryDetailsPage/EntryDetailsPage.tsx
@@ -3,27 +3,27 @@ import { useParams, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { VStack, HStack } from 'react-swiftstacks';
 
-import { useEntryForm } from '../../hooks/useEntryForm.ts';
+import { useEntryForm } from '../../hooks/useEntryForm';
 import {
     deleteUnwrapped,
     getUnwrappedWithParams,
     patchUnwrapped,
-} from '../../utils/axiosInstance.ts';
+} from '../../utils/axiosInstance';
 
-import InputField from '../../components/Forms/InputField/InputField.tsx';
-import DateTimePicker from '../../components/Forms/DateTimePicker/DateTimePicker.tsx';
-import Selector from '../../components/Forms/Selector/Selector.tsx';
-import Slider from '../../components/Forms/Slider/Slider.tsx';
-import StdButton from '../../components/Buttons/StdButton/StdButton.tsx';
+import InputField from '../../components/Forms/InputField/InputField';
+import DateTimePicker from '../../components/Forms/DateTimePicker/DateTimePicker';
+import Selector from '../../components/Forms/Selector/Selector';
+import Slider from '../../components/Forms/Slider/Slider';
+import StdButton from '../../components/Buttons/StdButton/StdButton';
 
-import { gameStatus } from '../../data/status.ts';
-import { gamingPlatforms } from '../../data/platforms.ts';
+import { gameStatus } from '../../data/status';
+import { gamingPlatforms } from '../../data/platforms';
 
-import type { EntryFormData } from '../../types/entryForm.ts';
-import { API_BASE_URL } from '../../config/apiURL.ts';
-import LoadingCircle from '../../components/LoadingCircle/LoadingCircle.tsx';
-import ConfirmModal from '../../components/ConfirmModal/ConfirmModal.tsx';
-import makeClearHandler from '../../utils/makeClearHandler.ts';
+import type { EntryFormData } from '../../types/entryForm';
+import { API_BASE_URL } from '../../config/apiURL';
+import LoadingCircle from '../../components/LoadingCircle/LoadingCircle';
+import ConfirmModal from '../../components/ConfirmModal/ConfirmModal';
+import makeClearHandler from '../../utils/makeClearHandler';
 
 interface PatchResponse {
     message: string;
@@ -51,13 +51,13 @@ const EntryDetailsPage = () => {
         validate,
     } = useEntryForm();
 
-    const [initialLoading, setInitialLoading] = useState(true);
+    const [isInitialLoading, setIsInitialLoading] = useState(true);
     const [isSubmitting, setIsSubmitting] = useState(false);
     const [isDeleting, setIsDeleting] = useState(false);
     const [showDeleteConfirm, setShowDeleteConfirm] = useState(false);
 
-    // Journal entry ID
     const { id } = useParams<{ id: string }>();
+    const navigate = useNavigate();
 
     const requiresRating = formData.status === 'completed';
     const hasRating = formData.rating !== null && formData.rating !== undefined;
@@ -69,14 +69,11 @@ const EntryDetailsPage = () => {
         formData.entryDate &&
         (!requiresRating || hasRating);
 
-    const navigate = useNavigate();
-
-    // Initial fetching
     useEffect(() => {
         if (!id) return;
 
         const fetchEntry = async () => {
-            setInitialLoading(true);
+            setIsInitialLoading(true);
 
             try {
                 const response =
@@ -96,39 +93,30 @@ const EntryDetailsPage = () => {
             } catch (e) {
                 toast.error((e as { message: string }).message);
             } finally {
-                setInitialLoading(false);
+                setIsInitialLoading(false);
             }
         };
 
         void fetchEntry();
     }, [id, setFormData]);
 
-    // PATCH payloads should always be explicit
     const payload = {
         ...(formData.title && { title: formData.title }),
         ...(formData.platform && { platform: formData.platform }),
         ...(formData.status && { status: formData.status }),
         ...(formData.entryDate && { entryDate: formData.entryDate }),
-
-        // Convert rating to number (slider outputs a string)
-        // The condition formating in rating is important to prevent sending 0
         ...(formData.rating != null && {
             rating: Number(formData.rating),
         }),
     };
 
     const handleClearTitle = makeClearHandler(setFormData, 'title', '');
-
     const handleClearPlatform = makeClearHandler(setFormData, 'platform', null);
-
     const handleClearStatus = makeClearHandler(setFormData, 'status', null);
-
     const handleClearRating = makeClearHandler(setFormData, 'rating', null);
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
-
-        if (!formData) return;
 
         if (!validate()) {
             toast.error('Fix the errors first!');
@@ -144,7 +132,6 @@ const EntryDetailsPage = () => {
             );
 
             toast.success(response.message);
-
             navigate('/journal');
         } catch (e) {
             toast.error((e as { message: string }).message);
@@ -163,11 +150,11 @@ const EntryDetailsPage = () => {
 
     const handleDeleteClick = async () => {
         if (!id) {
-            toast.error('No entry with give ID found.');
+            toast.error('No entry with given ID found.');
             return;
         }
 
-        setIsSubmitting(true);
+        setIsDeleting(true);
 
         try {
             const response = await deleteUnwrapped<EntryDetailsApiResponse>(
@@ -175,7 +162,6 @@ const EntryDetailsPage = () => {
             );
 
             toast.success(response.message);
-
             navigate('/journal');
         } catch (e) {
             toast.error((e as { message: string }).message);
@@ -185,125 +171,118 @@ const EntryDetailsPage = () => {
         }
     };
 
+    if (isInitialLoading) {
+        return (
+            <div className="fullscreenLoader">
+                <LoadingCircle />
+            </div>
+        );
+    }
+
     return (
         <VStack>
-            {initialLoading || !formData ? (
-                <div className="fullscreenLoader">
-                    <LoadingCircle />
-                </div>
-            ) : (
-                <form onSubmit={handleSubmit}>
-                    <VStack
-                        gap="lg"
-                        style={{ maxWidth: '400px' }}
-                        padding="md"
-                        className="formVStack"
+            <form onSubmit={handleSubmit}>
+                <VStack
+                    gap="lg"
+                    style={{ maxWidth: '400px' }}
+                    padding="md"
+                    className="formVStack"
+                >
+                    <InputField
+                        label="Title"
+                        id="title"
+                        name="title"
+                        type="text"
+                        value={formData.title}
+                        placeholder="Enter a title..."
+                        onChange={handleChange}
+                        onClear={handleClearTitle}
+                        error={errors.title}
+                    />
+
+                    <Selector
+                        label="Platform"
+                        id="platform"
+                        name="platform"
+                        size={1}
+                        value={formData.platform ?? ''}
+                        values={gamingPlatforms}
+                        placeholder="Select a platform..."
+                        onChange={handleChange}
+                        onClear={handleClearPlatform}
+                        error={errors.platform}
+                    />
+
+                    <Selector
+                        label="Status"
+                        id="status"
+                        name="status"
+                        size={1}
+                        value={formData.status ?? ''}
+                        values={gameStatus}
+                        placeholder="Select status..."
+                        onChange={handleChange}
+                        onClear={handleClearStatus}
+                        error={errors.status}
+                    />
+
+                    <DateTimePicker
+                        label="Date"
+                        id="entryDate"
+                        name="entryDate"
+                        value={formData.entryDate}
+                        onChange={handleDateChange}
+                        showTime
+                        isInvalid={!!errors.entryDate}
+                        errorMessage={errors.entryDate}
+                    />
+
+                    <Slider
+                        label="Rating"
+                        name="rating"
+                        min={0}
+                        max={10}
+                        value={formData.rating}
+                        onChange={handleChange}
+                        onClear={handleClearRating}
+                        disabled={formData.status !== 'completed'}
+                    />
+
+                    <HStack
+                        justify="center"
+                        gap="md"
+                        style={{ marginTop: '1rem' }}
                     >
-                        <InputField
-                            label="Title"
-                            id="title"
-                            name="title"
-                            type="text"
-                            value={formData.title}
-                            placeholder="Enter a title..."
-                            onChange={handleChange}
-                            onClear={handleClearTitle}
-                            error={errors.title}
-                        />
-
-                        <Selector
-                            label="Platform"
-                            id="platform"
-                            name="platform"
-                            size={1}
-                            value={formData.platform ?? ''}
-                            values={gamingPlatforms}
-                            placeholder="Select a platform..."
-                            onChange={handleChange}
-                            onClear={handleClearPlatform}
-                            error={errors.platform}
-                        />
-
-                        <Selector
-                            label="Status"
-                            id="status"
-                            name="status"
-                            size={1}
-                            value={formData.status ?? ''}
-                            values={gameStatus}
-                            placeholder="Select status..."
-                            onChange={handleChange}
-                            onClear={handleClearStatus}
-                            error={errors.status}
-                        />
-
-                        <DateTimePicker
-                            label="Date"
-                            id="entryDate"
-                            name="entryDate"
-                            value={formData.entryDate}
-                            onChange={handleDateChange}
-                            showTime
-                            isInvalid={!!errors.entryDate}
-                            errorMessage={errors.entryDate}
-                        />
-
-                        <Slider
-                            label="Rating"
-                            name="rating"
-                            min={0}
-                            max={10}
-                            value={formData.rating}
-                            onChange={handleChange}
-                            onClear={handleClearRating}
-                            disabled={formData.status !== 'completed'}
-                        />
-
-                        <HStack
-                            justify={'center'}
-                            gap={'md'}
-                            style={{ marginTop: '1rem' }}
+                        <StdButton
+                            type="button"
+                            onClick={handleCancelClick}
+                            width="150px"
                         >
-                            <StdButton
-                                type={'button'}
-                                onClick={handleCancelClick}
-                                width={'150px'}
-                            >
-                                Cancel
-                            </StdButton>
-                            {!isSubmitting ? (
-                                <StdButton
-                                    type="submit"
-                                    disabled={!isFormReady || isSubmitting}
-                                    width={'150px'}
-                                    color={'green'}
-                                >
-                                    Save
-                                </StdButton>
-                            ) : (
-                                <StdButton
-                                    type="submit"
-                                    disabled={!isFormReady || isSubmitting}
-                                    width={'150px'}
-                                >
-                                    Saving...
-                                </StdButton>
-                            )}
-                        </HStack>
+                            Cancel
+                        </StdButton>
 
-                        <HStack justify={'center'}>
-                            <StdButton
-                                type={'button'}
-                                onClick={() => setShowDeleteConfirm(true)}
-                                color={'red'}
-                                width={'150px'}
-                            >
-                                Delete
-                            </StdButton>
-                        </HStack>
-                    </VStack>
-                </form>
-            )}
+                        <StdButton
+                            type="submit"
+                            disabled={!isFormReady || isSubmitting}
+                            width="150px"
+                            color="green"
+                        >
+                            {isSubmitting ? 'Saving…' : 'Save'}
+                        </StdButton>
+                    </HStack>
+
+                    <HStack justify="center">
+                        <StdButton
+                            type="button"
+                            onClick={() => setShowDeleteConfirm(true)}
+                            color="red"
+                            width="150px"
+                        >
+                            Delete
+                        </StdButton>
+                    </HStack>
+                </VStack>
+            </form>
 
             <ConfirmModal
                 open={showDeleteConfirm}
@@ -318,4 +297,5 @@ const EntryDetailsPage = () => {
         </VStack>
     );
 };
+
 export default EntryDetailsPage;
