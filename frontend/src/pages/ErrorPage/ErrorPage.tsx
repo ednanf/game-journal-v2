895ca4fdd2d5d8c6220d332fd6a0477c17c538b9
Ednan Rogério Frizzera Filho <ednan@frizzera.dev>
2026-02-10 09:32:23 -0300

    feature: implement simple 404 page
    Signed-off-by: Ednan Rogério Frizzera Filho <ednan@frizzera.dev>

--- frontend/src/pages/ErrorPage/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage/ErrorPage.tsx
@@ -1,6 +1,20 @@
+import { VStack } from 'react-swiftstacks';
+import { useNavigate } from 'react-router-dom';
+import StdButton from '../../components/Buttons/StdButton/StdButton.tsx';
+
 const ErrorPage = () => {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate(-1);
+    };
+
     return (
-        <div>ErrorPage</div>
+        <VStack align={'center'} padding={'xl'} gap={'lg'}>
+            <h2>404 - Page Not Found</h2>
+            <p>The requested page does not exist.</p>
+            <StdButton onClick={handleClick}>Go Back</StdButton>
+        </VStack>
     );
 };
 export default ErrorPage;
